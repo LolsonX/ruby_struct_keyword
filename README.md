da9c0770fd36ff376278939474341de3a7cace90
Karol Ostrowski <xlolsonx@gmail.com>
2024-12-04 07:10:17 +0100

    Keyword 'struct' in Ruby

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # ruby_struct_keyword
 Experiment how flexible ruby can be
+
+## Why
+I just wanted to try out how flexible ruby can be. And if I can create something that feels as a natural part of language within that language. There are no dependencies, no gems, no magic, just a simple DSL. But with usage of `const_missing` and `caller` I was able to create something that feels like a natural part of the language.
+## Usage
+
+```ruby
+require 'ruby_struct_keyword'
+
+struct User do
+  property :name
+end
+
+user = User.new(name: 'Karolo')
+user.name # => 'Karolo'
+```
+
+## Installation
+
+Download or copy the file `ruby_struct_keyword.rb` into your project.
+
+## License
+
+The gem is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
---
+++ ruby_struct_keyword.rb
@@ -0,0 +1,44 @@
+class Object
+  def struct(name, &blk)
+    @properties = []
+    def property(name)
+      @properties << name
+    end
+    location = caller(1..1).first
+
+    line_content = if location.include?('(irb)')
+                     IRB.CurrentContext.io.line(-1).chomp
+                   else
+                     # For non-IRB usage, fall back to file reading
+                     file, line_number = location.split(':')[0..1]
+
+                     # Read the file to get the content of the specific line
+                     File.readlines(file)[line_number.to_i - 1].strip
+                   end
+    const_sym = line_content.split(' ')[1].to_sym
+    instance_exec(&blk)
+    Object.const_set(const_sym, Struct.new(*@properties))
+  end
+
+  def self.const_missing(name)
+    # Get the location of the constant reference
+    location = caller(1..1).first
+
+    line_content = if location.include?('(irb)')
+                     IRB.CurrentContext.io.line(-1).chomp
+                   else
+                     # For non-IRB usage, fall back to file reading
+                     file, line_number = location.split(':')[0..1]
+
+                     # Read the file to get the content of the specific line
+                     File.readlines(file)[line_number.to_i - 1].strip
+                   end
+
+    # Optionally, raise an error or define the constant
+    super unless line_content.start_with?('struct ')
+  end
+end
+
+struct User do
+  property :name
+end
